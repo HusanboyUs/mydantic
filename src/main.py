601628cc76f4921997e66de57d410e2f90abd0d3
Husanboy Usmonov <70463121+HusanboyUs@users.noreply.github.com>
2025-04-21 15:15:54 +0200

    Dev0.1 (#7)
    * re-writing validation logic for dev0.1
    
    * Re-wrtitng logic to cover more scenarios and features
    
    * Added test cases for base class
    
    * cleaned example file

--- src/main.py
+++ src/main.py
@@ -11,23 +11,57 @@ class BaseModel:
     def __init__(self, **kwargs) -> None:
         for key, values in kwargs.items():
             if key in self.__annotations__:
-                self.__setattr__(key,values)
+                has_default = self._has_default(key=key)
+                if has_default[0]:
+                    self.__setattr__(key,has_default[1])
+                else:
+                    self.__setattr__(key,values)
 
         self._validate_fields(**kwargs)
-
+    
+    @classmethod
+    def _has_default(cls, key) -> list[bool,str:None]:
+        """Checks if class variable has a default value assigned, returns True if it has False otherwise"""
+        if key not in cls.__dict__:
+            return [False]
+        return [True, cls.__dict__[key]]
+    
     @classmethod
     def _validate_fields(cls, **kwargs) -> bool | Exception:
         """Class based method to validate the field types"""
 
-        element = lambda value: kwargs.get(value)
+        _external_values = kwargs
 
         for native_key, native_type in cls.__annotations__.items():
 
-            current_value = element(native_key)
-            if current_value is None:
-                raise FieldValidationError(field=f"{native_key}", message=f"{native_key} returned {current_value}")
+            # checking surface level only -> doesnt work for values inside of nested dicts
+            if native_key not in kwargs.keys():
+                if not cls._has_default(key=native_key)[0]:
+                    raise FieldValidationError(
+                        field=native_key, 
+                        message=f"Given field -> {native_key} is defined in class but not provided in payload"
+                    )
+            
+            # print(f" args: {get_args(native_type)} for {native_type} {native_key}")
+            # print(f" origin: {get_origin(native_type)} for {native_type} {native_key}")
 
-            if not isinstance(current_value, native_type):
-                raise FieldValidationError(field=f"{native_key}", message=f"{native_key} does not much with {current_value} type!")
+            #if nested - checking only type of the values in nested list
+            if get_origin(native_type):
 
-        return True
+                for value in cls.__annotations__.get(native_key):
+                    print(f"Value is ------------------- {value}")
+                    if not isinstance(value, dict):
+                        pass
+
+                    if any(isinstance(value, t) and isinstance(value, t) for t in native_key):
+                        pass
+
+                
+
+                break
+
+            # if not nested
+            if not isinstance(_external_values.get(native_key), native_type):
+                raise FieldValidationError(
+                        field=_external_values.get(native_key),
+                        message=f"{_external_values.get(native_key)} did not match with {native_type}")
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,13 +3,54 @@
 from unittest import TestCase
 #module under test
 from src import BaseModel
+from src import FieldValidationError
 
 
-class TestBaseClass(TestCase):
+class Task(BaseModel):
 
-    """empty tests at least for now"""
-    pass
+    id:int
+    title: str
+    content: str
+    is_published: bool = False
 
 
-if __name__ == "__main__":
-    TestBaseClass.run()
+class TestBaseModelInitiations(TestCase):
+
+    def test_model_initiation_variables(self):
+        test_task_object = Task(
+            id=1,
+            title = "My First Task",
+            content="My task is to write a tests",
+            is_published = False
+        )
+        self.assertEqual(test_task_object.id) == Task.id
+        self.assertEqual(test_task_object.title) == Task.title
+        self.assertEqual(test_task_object.content) == Task.content
+    
+    def test_model_default_values(self):
+        test_task_object = Task(
+            id=1,
+            title = "My First Task",
+            content="My task is to write a tests",
+            is_published = True
+        )
+        self.assertFalse(test_task_object.is_published)
+
+    def test_missing_values(self):
+        with self.assertRaises([FieldValidationError]):
+            test_task_object = Task(
+            id=1,
+            content="My task is to write a tests",
+            is_published = True
+        )
+        
+    def test_check_fiel_types(self):
+        with self.assertRaises([FieldValidationError]) as errorMessage:
+            test_task_object = Task(
+                id='1',
+                title = "My First Task",
+                content="My task is to write a tests",
+                is_published = True
+            )
+        
+        self.assertEqual(errorMessage.msg,f'id did not match with str')
